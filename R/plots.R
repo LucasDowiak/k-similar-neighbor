f0488b01926b13bde85defcbf0c29ff5b51c77d6
LucasDowiak <lcdowi@gmail.com>
2021-07-14 19:40:59 -0400

    unify cor and dtw plot function calls

--- R/plots.R
+++ R/plots.R
@@ -2,35 +2,68 @@ library(ggplot2)
 library(data.table)
 setwd("~/Git/k-similar-neighbor/")
 
-plot_corr_matrix <- function(assoc_file, sub_ticks=NULL)
+
+# Read in the raw S&P 500 data file
+#
+#   ticks - optional subset of ticks to return
+#
+read_SandP_data <- function(ticks=NULL)
 {
-  
+  out <- fread("data/SandP_companies.csv")
+  out <- out[order(sector, subindustry, ticker)]
+  if (!is(ticks, "NULL"))
+    out <- out[ticker %in% ticks]
+  return(out)
+}
+
+
+# Plot the similarity matrices
+#
+#   assoc_file - file path for stored similarity matrix
+#
+#   type - type of similarity matrix, which effects certaing plot parameters
+#
+#   tits- optional title for the plot
+#
+plot_sim_matrix <- function(assoc_file, type=c("cor", "dtw"), tits="")
+{
+  require(ggplot2)
+  type <- match.arg(type)
+  if (type == "cor") {
+    midpoint <- 0
+    limit <- c(-1, 1)
+  } else if (type == "dtw") {
+    midpoint <- 100
+    limit <- c(0, 400)
+  }
   X <- read.table(assoc_file, header=TRUE, row.names=1)
-  tmp <- fread("data/SandP_companies.csv")
-  tmp <- tmp[order(sector, subindustry, ticker)][ticker %in% names(X)]
-  tmp[, idx := 1:.N]
-  X <- X[tmp$ticker, tmp$ticker]
+  dtfStock <- read_SandP_data(names(X))
+  dtfStock[, idx := 1:.N]
+  
+  X <- X[dtfStock$ticker, dtfStock$ticker]
   X$ticks <- names(X)
   X$ticks <- factor(X$ticks, levels=X$ticks)
+  
   X <- as.data.table(X)
-  idxsec <- which(tmp[1:(.N - 1), sector] != tmp[2:.N, sector])
-  idxlab <- tmp[, unique(sector)]
-  idxloc <- tmp[, round(mean(idx)), by=sector][, V1]
+  idxsec <- which(dtfStock[1:(.N - 1), sector] != dtfStock[2:.N, sector])
+  idxlab <- dtfStock[, unique(sector)]
+  idxloc <- dtfStock[, round(mean(idx)), by=sector][, V1]
   
   X <- melt(X, id.vars="ticks", measure.vars=setdiff(names(X), "ticks"))
-  ggplot(data=X, aes(x=ticks, y=variable, fill=value)) +
+  
+  p <- ggplot(data=X, aes(x=ticks, y=variable, fill=value)) +
     geom_tile(color = "white") +
     scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
-                         midpoint = 0, limit = c(-1,1), space = "Lab", 
-                         name="Pearson\nCorrelation") +
+                         midpoint = midpoint, limit = limit, space = "Lab", 
+                         name="") +
     theme_minimal() +
     #theme(axis.text.x=element_text(angle = -15)) +
     theme(axis.text=element_blank()) +
     annotate(geom="text", x=idxloc, y=idxloc, label=idxlab) +
-    scale_x_discrete(breaks=tmp$ticker[idxloc], labels=idxlab) + 
-    scale_y_discrete(breaks=tmp$ticker[idxloc], labels=idxlab) + 
+    scale_x_discrete(breaks=dtfStock$ticker[idxloc], labels=idxlab) + 
+    scale_y_discrete(breaks=dtfStock$ticker[idxloc], labels=idxlab) + 
     geom_vline(xintercept=idxsec, size=0.5, lty=3) + 
-    geom_hline(yintercept=idxsec, size=0.5, lty=3)
+    geom_hline(yintercept=idxsec, size=0.5, lty=3) +
+    labs(x="", y="", title=tits)
+  print(p)
 }
-
-plot_corr_matrix("data/association_results/corr_20190201_20200214_483")
