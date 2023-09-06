188815dd9b8a2e75271323b56ee81c625ec93194
LucasDowiak <lcdowi@gmail.com>
2023-09-06 10:53:44 -0400

    updated batch opt method

--- R/auto_marginal_script.R
+++ R/auto_marginal_script.R
@@ -9,49 +9,35 @@ source("R/auto_marginal.R")
 # ------------------------------------------------------------------------
 # code snippet to run through all S&P 500 stock tickers for a given year
 
-run_arima_garch_by_years <- function(years, rerun=TRUE)
-{
-  if (length(years) > 1) {
-    year_range <- paste0(years, collapse="_")
-  } else {
-    year_range <- as.character(years)
-  }
-  tickers <- sapply(strsplit(dir("data/raw_json", pattern="json$"), "\\."), `[[`, 1)
-  spec_path <- sprintf("data/marginal_specifications_%s.json", year_range)
-  
-  if (rerun) {
-    spec_list <- interpret_spec_list(spec_path)
-    tmp <- good_vs_bad_symbols(spec_path)
-    tickers <- c(tmp$fail_ticks, tmp$not_run_ticks)
-    
-  } else {
-    spec_list <- vector("list", length(tickers))
-    names(spec_list) <- tickers
-  }
-  
-  for (tick in tickers) {
-    print(sprintf("TICK %s at %s", tick, Sys.time()))
-    dtfU <- try(data.table(parse_json(tick)))
-    if (any(nrow(dtfU) == 0, length(dtfU) == 0, is(dtfU, "NULL"))) {
-      next
-    } else {
-      u <- dtfU[year(Date) %in% years, diff(log(close))]
-      out <- try(auto_fit(u))
-      if (inherits(out, "try-error"))
-        out <- as.character(out)
-      spec_list[[tick]] <- out
-      write_json(spec_list, path=spec_path)
-    }
-  }
-}
+# Function to run ARIMA-GARCH models by year
+#
+#   years - list of years to include as time series domain
+#
+#   rerun - boolean indicating if the process should start from the ticker where
+#           the previous run left off
+#
+debugonce(arima_garch_optimization)
+aa <- arima_garch_optimization(start_date="2019-01-01", end_date="2019-12-31",
+                         label="test",
+                         tickers=c("A", "NFLX", "GE"),
+                         write_output=TRUE,
+                         save_models=TRUE,
+                         rerun=FALSE)
 
-years <- c(2007, 2008)
+# Run model optimization in batches ---------------------------------------------
+dtfSP <- fread("data/SandP_tick_history.csv")
+st_date <- "2019-01-01"
+ed_date <- "2020-12-31"
 
-run_arima_garch_by_years(c(2007, 2008), rerun=FALSE)
+tick_bool <- dtfSP[Date >= st_date & Date <= ed_date, sapply(.SD, function(x) !any(is.na(x)))]
+ticks_2019_2020 <- setdiff(names(tick_bool[tick_bool]), "Date")
+aa <- arima_garch_optimization(start_date=st_date, end_date=ed_date,
+                               label="DA_2019_2020",
+                               tickers=ticks_2019_2020,
+                               write_output=TRUE,
+                               save_models=TRUE)
 
-for (yr in years) {
-  run_arima_garch_by_year(yr, rerun = FALSE)
-}
+# QA on the model selection process --------------------------------------------
 
 lst_files <- list.files('data', pattern = "marginal_specifications_[0-9]{4}.json",
                         full.names = TRUE)
@@ -61,7 +47,10 @@ names(o) <- lst_files
 
 all_fails <- Reduce(union, o)
 univ_intersect <- Reduce(intersect, lapply(gvb, `[[`, 'pass_ticks'))
-gvb_sums <- t(sapply(gvb, function(x) c(pass_ticks=length(x$pass_ticks), fail_ticks=length(x$fail_ticks), not_run=length(x$not_run_ticks))))
+gvb_sums <- t(sapply(gvb,
+                     function(x) c(pass_ticks=length(x$pass_ticks),
+                                   fail_ticks=length(x$fail_ticks),
+                                   not_run=length(x$not_run_ticks))))
 
 
 SPEC_PATH <- "data/marginal_specifications_20200323_20210219.json"
@@ -83,7 +72,7 @@ dtf <- dcast(dtf, Date ~ tick, value.var="close")
 dtf <- dtf[, .SD, .SDcols=c(dtfMinDate$tick, "Date")]
 fwrite(dtf, file="data/SandP_tick_history.csv")
 # Missing map
-Amelia::missmap(dtf, ylab="Feb 2021 - Nov 1999",
+Amelia::missmap(dtfSP, ylab="Feb 2021 - Nov 1999",
                 xlab="Tick")
 
 
@@ -123,12 +112,5 @@ for (tick in names(xx[xx==1])) {
 }
 
 
-produce_rGARCHfit_from_spec("ABC", SPECIFICATIONS)
 
-out_spec <- run_seasonal_modifications(badticks, SPECS)
-
-write_json(out_spec, path="data/marginal_specifications_seasonal.json")
-
-tmp1 <- fread("data/std_resids_20190201_20200214.csv")
-tmp2 <- fread("data/std_resids_20200323_20210219.csv")
 
