75d668fb0b3c8e6265cfbc40d4bf6528371ba8de
LucasDowiak <Lcdowi@gmail.com>
2023-10-16 13:37:06 -0400

    change api endpoint to adj close

--- R/SandP_data.R
+++ R/SandP_data.R
@@ -1,5 +1,6 @@
 setwd("~/Git/k-similar-neighbor/")
 source("R/alpha_adv_api_key.R")
+source("R/auto_marginal.R")
 library(rvest)
 library(RCurl)
 library(jsonlite)
@@ -9,8 +10,8 @@ u <- "https://en.wikipedia.org/wiki/List_of_S&P_500_companies"
 doc <- read_html(u)
 tables <- html_nodes(doc, "table")
 dtfCos <- html_table(tables[1])[[1]]
-names(dtfCos) <- c("ticker", "security", "secfilings", "sector", "subindustry",
-                   "hq", "added_date", "cik", "some_year")
+names(dtfCos) <- c("ticker", "security", "GICS_sector", "GICS_sub_ind",
+                   "hq", "date_added", "cik", "founded")
 
 
 # Use stock ticker to pull data from alpha advantage api
@@ -27,7 +28,7 @@ hit_api <- function(tickers, outputsize=c("compact", "full"), saveJSON=FALSE)
   }
   
   baseuri <- "https://www.alphavantage.co/query"
-  parstring <- "function=TIME_SERIES_DAILY&symbol=%s&outputsize=%s&apikey=%s"
+  parstring <- "function=TIME_SERIES_DAILY_ADJUSTED&symbol=%s&outputsize=%s&apikey=%s"
   uri <- paste(baseuri, 
                sprintf(parstring, tickers, outputsize, .APIKEY),
                sep="?")
@@ -76,17 +77,36 @@ list_all_available <- function()
   return(read.csv(textConnection(rawToChar(aa))))
 }
 
+
+# Run through list of stock ticks and obtain data
+# ------------------------------------------------------------------------------
+
 for (tick in dtfCos$ticker) {
+  cat('\r', sprintf("Tick: %s - %d / %d", tick, which(tick == dtfCos$ticker), nrow(dtfCos)))
   hit_api(tick, outputsize="full", saveJSON=TRUE)
-  Sys.sleep(15)
+  Sys.sleep(2.1)
 }
 
 
-files <- dir("data/", pattern="*.json")
+# Create single data.frame with stock tick values as columns
+lst_dtfs <- lapply(dtfCos$ticker, parse_stock_data, 5) # 5 represents adjusted closing price
+dtf <- rbindlist(lapply(lst_dtfs, function(DT) {DT[, "tick"] <- attr(DT, "ticker"); return(DT)}))
+
+# Order stocks by length of history
+dtfMinDate <- dtf[, min(Date), by=tick][order(V1)]
+dtf <- dcast(dtf, Date ~ tick, value.var="adjusted close")
+dtf <- dtf[, .SD, .SDcols=c(dtfMinDate$tick, "Date")]
+fwrite(dtf, file="data/SandP_tick_history.csv")
+
+# Missing map
+Amelia::missmap(dtf, ylab="Feb 2021 - Nov 1999", xlab="Tick")
+
+
+files <- dir("data/raw_json/", pattern="*.json")
 
 # Check for failed data pulls and re-run data pull from AlphaAdvantage API
 for (f_ in files) {
-  dir_ <- paste0("data/", f_)
+  dir_ <- paste0("data/raw_json/", f_)
   fs <- file.size(dir_)
   if (fs < 300) {
     print(sprintf("Re-running %s", dir_))
@@ -98,7 +118,7 @@ for (f_ in files) {
     } else {
       print(sprintf("Not Cured %s", dir_))
     }
-    Sys.sleep(15)
+    Sys.sleep(2.01)
   }
 }
 
