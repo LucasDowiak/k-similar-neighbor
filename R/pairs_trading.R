d8fdf8ded43ef848c1f7912a825d73f3dc9c4e55
LucasDowiak <lcdowi@gmail.com>
2023-11-28 15:49:52 -0500

    update and refine pairs trading

--- R/pairs_trading.R
+++ R/pairs_trading.R
@@ -1,16 +1,20 @@
-source("R/similarity_functions.R")
-
 # Pairs-Trading ----------------------------------------------------------------
 # Find the N most closely associated pairs
-find_pairs <- function(M, k=10L, dist.method=c("cor", "dtw"))
+find_pairs <- function(M, k=10L, si=1, dist.method=c("cor", "dtw"), omit_ticks=NULL)
 {
   # M - association matrix
   dist.method <- match.arg(dist.method)
+  
+  if (!is.null(subset)) {
+    cn <- colnames(M)
+    cn <- cn[!cn %in% omit_ticks]
+    M <- M[cn, cn]
+  }
   vals <- sort(M[lower.tri(M)])
   if (dist.method == "cor") {
-    topn <- rev(tail(vals, n=k))
+    topn <- rev(vals)[si:(si + k - 1L)]
   } else {
-    topn <- head(vals, n=k) 
+    topn <- vals[si:(si + k - 1L)]
   }
   out <- lapply(topn, function(x) row.names(which(M == x, arr.ind=TRUE)))
   attr(out, 'summary') <- summary(vals)
@@ -18,6 +22,7 @@ find_pairs <- function(M, k=10L, dist.method=c("cor", "dtw"))
   attr(out, "dist.method") <- dist.method
   return(out)
 }
+
 
 .pull_ticks_and_melt <- function(pair, DT)
 {
@@ -28,8 +33,10 @@ find_pairs <- function(M, k=10L, dist.method=c("cor", "dtw"))
   return(out)
 }
 
+
 plot_list_of_pairs <- function(pairs, DT)
 {
+  require(ggplot2)
   dtfplot <- rbindlist(lapply(pairs, .pull_ticks_and_melt, DT=DT))
   fpairs <- sapply(pairs, function(x) paste(x, collapse="-"))
   p <- ggplot() +
@@ -45,47 +52,48 @@ open_position_signal <- function(d, Z, threshold=2)
   return(z > threshold)
 }
 
+
 close_position_signal <- function(d)
 {
   return(c(NA, diff(sign(d)) != 0))
 }
 
-dtfFormCumRet <- fread(sprintf("data/label_analysis/%s_std_price.csv", "2012"))
-dtfTradeCumRet <- fread(sprintf("data/label_analysis/%s_std_price.csv", "2013"))
 
-aa_dtw <- read_association_table("data/association_results/2012_dtw.tsv")
-aa_udj <- read_association_table("data/association_results/2012_unadjusted_cor.tsv")
-aa_adj <- read_association_table("data/association_results/2012_model_cor.tsv")
-
-pairs_udj <- find_pairs(aa_udj, n=10L)
-pairs_adj <- find_pairs(aa_adj, n=10L)
-pairs_dtw <- find_pairs(aa_dtw, n=10L, dist.method = "dtw")
-
-
-
-back_test_strategy <- function(label, k=10L, buy_signal=c("unadjusted_cor", "model_cor", "dtw"))
+.calculate_return <- function(DT, short_premium=0.025)
 {
-  buy_signal <- match.arg(buy_signal)
-  trade_year <- as.character(as.integer(label) + 1)
+  t1 <- DT[, unique(tick1)]; t2 <- DT[, unique(tick2)]
+  strike_prices <- DT[action=="open", unlist(.SD), .SDcols=c(t1, t2)]
+  spot_prices <- DT[action=="close", unlist(.SD), .SDcols=c(t1, t2)]
+  long_pos <- names(which.min(strike_prices))
+  short_pos <- names(which.max(strike_prices))
   
-  # Stat and train on 2012 data
-  ## Given buy-signal read in association matrix
-  assoc_file <- sprintf("data/association_results/%s_%s.tsv", label, buy_signal)
-  M <- read_association_table(assoc_file)
-  dtfFormCumRet <- fread(sprintf("data/label_analysis/%s_std_price.csv", label))
-  dtfTradeCumRet <- fread(sprintf("data/label_analysis/%s_std_price.csv", trade_year))
-  Tf <- dtfFormCumRet[, .N]
+  long_ret <- spot_prices[long_pos] - strike_prices[long_pos]
+  short_ret <- strike_prices[short_pos] - spot_prices[short_pos]
+  return(data.table(pair=DT[1, pair],
+                    open_date=DT[action=="open", Date],
+                    close_date=DT[action=="close", Date],
+                    long_pos=long_pos,
+                    long_strike=strike_prices[long_pos],
+                    long_spot=spot_prices[long_pos],
+                    long_ret=long_ret,
+                    short_pos=short_pos,
+                    short_strike=strike_prices[short_pos],
+                    short_spot=spot_prices[short_pos],
+                    short_ret=short_ret,
+                    pair_return=long_ret + short_ret))
+}
+
+
+back_test_pair <- function(pairs, dt_form, dt_trade, threshold=2)
+{
+  t1 <- pairs[1]; t2 <- pairs[2]
+  rf1 <- dt_form[[t1]]; rf2 <- dt_form[[t2]]
+  rt1 <- dt_trade[[t1]]; rt2 <- dt_trade[[t2]]
+  open_index <- which(open_position_signal(rt1 - rt2, scale(abs(rf1 - rf2)),
+                                           threshold=threshold))
+  close_index <- which(close_position_signal(rt1 - rt2))
+  pname <- paste(pairs, collapse="-")
   
-  ## Find the top K pairs
-  dm <- if (buy_signal == "dtw") "dtw" else "cor"
-  top_pairs <- find_pairs(M, k=k, dist.method=dm)
-  
-  # Execute strategy on following year's returns
-  ## Read in standard price in 2013
-  out <- vector("list", length=k)
-  names(out) <- sapply(top_pairs, paste, collapse="-")
-  
-  .open_position()
   .close_position <- function(op, cls)
   {
     if (any(cls[cls > op$timet])) {
@@ -97,76 +105,119 @@ back_test_strategy <- function(label, k=10L, buy_signal=c("unadjusted_cor", "mod
     return(rbindlist(list(op, cp)))
   }
   
-  for (ii in seq_len(k)) {
-    t1 <- top_pairs[[ii]][1]
-    t2 <- top_pairs[[ii]][2]
-    pname <- paste(top_pairs[[ii]], collapse="-")
-    
-    rf1 <- dtfFormCumRet[year(Date) == as.integer(label)][[t1]]
-    rf2 <- dtfFormCumRet[year(Date) == as.integer(label)][[t2]]
-    rt1 <- dtfTradeCumRet[year(Date) == as.integer(trade_year)][[t1]]
-    rt2 <- dtfTradeCumRet[year(Date) == as.integer(trade_year)][[t2]]
-    
-    open_index <- which(open_position_signal(rt1 - rt2, scale(abs(rf1 - rf2))))
-    close_index <- which(close_position_signal(rt1 - rt2))
-    
-
-    operations <- list()
-    tt <- 0
-    while (tt < Tf) {
-      # If there is an open position signal
-      if (length(open_index) > 0) {
-        # Open the position
-        oi <- open_index[1]
-        open_position <- list(action="open", timet=oi, trade_id=as.character(round(runif(1), 15) * 1e15))
-        
-        # Close the position
-        trade <- .close_position(open_position, close_index)
-        operations[[length(operations) + 1]] <- trade
-        
-        # Increment loop logic
-        tt <- trade[action=="close", timet] + 1
-        open_index <- open_index[open_index >= tt]
-      } else {
-        break
-      }
+  operations <- list()
+  Tf <- dt_trade[, .N]
+  tt <- 0
+  while (tt < Tf) {
+    # If there is an open position signal
+    if (length(open_index) > 0) {
+      # Open the position
+      oi <- open_index[1]
+      open_position <- list(action="open", timet=oi, trade_id=as.character(round(runif(1), 15) * 1e15))
+      
+      # Close the position
+      trade <- .close_position(open_position, close_index)
+      # aa <- excess_return(trade, dtfTradeCumRet)
+      operations[[length(operations) + 1]] <- trade
+      
+      # Increment loop logic
+      tt <- trade[action=="close", timet] + 1
+      open_index <- open_index[open_index >= tt]
+    } else {
+      break
     }
+  }
+  if (length(operations) == 0L) {
+    return(data.table())
+  } else {
     pair_trades <- rbindlist(operations)
-    pair_trades[, `:=`(pair = pname, tick1=t1, tick2=t2)]
-    
-    tmp <- dtfFormCumRet[pair_trades$timet, .SD, .SDcols=c('Date', t1, t2)]
+    pair_trades[, `:=`(pair=pname, tick1=t1, tick2=t2)]
+    tmp <- dt_trade[pair_trades$timet, .SD, .SDcols=c('Date', t1, t2)]
     pair_trades <- cbind(pair_trades, tmp)
-    out[[pname]] <- pair_trades
+    revenues <- pair_trades[, .calculate_return(.SD), by=trade_id]
   }
-  return(out)
-}
-
-.position <- function(type=c("open", "close"), prices)
-
-calculate_returns <- function(bts, returns)
-{
   
-  position <- NULL
-  nms <- names(bts)
-  for (k in seq_along(bts)) {
-    t1 <- unlist(strsplit(nms[[k]], "-"))[1]
-    t2 <- unlist(strsplit(nms[[k]], "-"))[2]
-    
-    for (a in seq_along(bts[[k]])) {
-      p <- dtfTradeCumRet[bts[[k]][[a]]$timet, .SD, .SDcols=c(t1, t2)]
-      position <- .position(bts[[k]][[a]]$action, prices, position)
-      wmax <- which.max(prices)
-    }
-
-
-  }
+  
+  return(revenues)
 }
 
 
+back_test_strategy <- function(label, k=10L, start_index=1, threshold=2, buy_signal=c("unadjusted_cor", "model_cor", "dtw"))
+{
+  buy_signal <- match.arg(buy_signal)
+  trade_year <- as.character(as.integer(label) + 1)
+  # Stat and train on 2012 data
+  ## Given buy-signal read in association matrix
+  assoc_file <- sprintf("data/association_results/%s_%s.tsv", label, buy_signal)
+  M <- read_association_table(assoc_file)
+  dtfFormCumRet <- fread(sprintf("data/label_analysis/%s_std_price.csv", label))
+  dtfTradeCumRet <- fread(sprintf("data/label_analysis/%s_std_price.csv", trade_year))
+  Tf <- dtfTradeCumRet[, .N]
+  bad_ticks <- colnames(M)[!colnames(M) %in% names(dtfTradeCumRet)]
+  
+  ## Find the top K pairs
+  dm <- if (buy_signal == "dtw") "dtw" else "cor"
+  top_pairs <- find_pairs(M, k=k, si=start_index, dist.method=dm, omit_ticks=bad_ticks)
+  pnames <- unlist(lapply(top_pairs, paste, collapse="-"))
+  # Execute strategy on following year's returns
+  ## Read in standard price in 2013
+  revn <- vector("list", length=k)
+  names(revn) <- sapply(top_pairs, paste, collapse="-")
+  
+  for (ii in seq_len(k)) {
+    pn <- paste(top_pairs[[ii]], collapse="-")
+    revn[[pn]] <- back_test_pair(top_pairs[[ii]], dtfFormCumRet, dtfTradeCumRet,
+                                 threshold=threshold)
+  }
+  
+  # What annual return do we get by going long in the portfolio for the year
+  bl_long <- sapply(top_pairs, base_line_long_return, dt_trade=dtfTradeCumRet)
+  
+  # Return metrics by pair
+  dtfPairEval <- data.table(pair=pnames)
+  if (all(sapply(revn, length) == 0)) {
+    revenues <- data.table()
+    dtfPairEval[, V1 := NA_real_]
+    commited_return <- invested_return <- 1
+  } else {
+    revenues <- rbindlist(revn)[order(pair, close_date)]
+    rev_by_pair <- revenues[, prod(1 + pair_return), by=pair]
+    dtfPairEval <- merge(dtfPairEval, rev_by_pair, all.x=TRUE)
+    # Provide invested and committed return values
+    invested_return <- dtfPairEval[, mean(V1, na.rm=TRUE)]
+    commited_return <- dtfPairEval[, (sum(V1, na.rm=TRUE) + sum(is.na(V1))) / k]
+  }
+  dtfPairEval[, trade_year := trade_year]
+  dtfPairEval[, threshold := threshold]
+  dtfPairEval[, start_index := start_index]
+  dtfPairEval[, n_pairs := k]
+  dtfPairEval[, buy_signal := buy_signal]
+  
+  revenues[, trade_year := trade_year]
+  revenues[, threshold := threshold]
+  revenues[, start_index := start_index]
+  revenues[, n_pairs := k]
+  revenues[, buy_signal := buy_signal]
+  
+  dtfBenchmarks = data.table(
+             trade_year=trade_year,
+             threshold=threshold,
+             start_index=start_index,
+             n_pairs=k,
+             buy_signal=buy_signal,
+             base_line_long=mean(bl_long),
+             committed=commited_return - 1,
+             invested=invested_return - 1)
+  
+  return(list(revenues=revenues, evaluation=dtfBenchmarks, pairs=dtfPairEval))
+}
 
-plot_list_of_pairs(pairs_dtw, dtfFormCumRet)
-plot_list_of_pairs(pairs_dtw, dtfTradeCumRet)
 
-tst <- back_test_strategy("2012", k=10, "dtw")
-#  - given buy-signal find open and close intervals for each pair through out the
-#    trading period
+base_line_long_return <- function(portfolio_pairs, dt_trade)
+{
+  pos <- unlist(lapply(portfolio_pairs, strsplit, "-"))
+  wts <- table(pos)
+  rtn <- dt_trade[.N, unlist(.SD), .SDcols=unique(pos)]
+  ann_ret <- sum((1 + rtn)[names(wts)] * wts) - sum(wts)
+  return(ann_ret)
+}
